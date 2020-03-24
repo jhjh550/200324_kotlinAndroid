a858eead30a5e4f9c5a2ac8b27dd59f6d78d0fd0
Your Name <you@example.com>
2020-03-24 16:44:21 +0900

    - ui widget

---
+++ app/src/main/java/com/example/myapplication/T02_UIWidget/T02_UIWidgetActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.T02_UIWidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_t02__u_i_widget.*
+
+class T02_UIWidgetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_t02__u_i_widget)
+
+        btnMyButton.setOnClickListener {
+            Toast.makeText(this, "hello button", Toast.LENGTH_SHORT).show()
+        }
+        myCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(this, "checked $isChecked", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+}
