668b24f353e2d0149200ac82526f1783203c1a03
Your Name <you@example.com>
2020-03-25 14:54:55 +0900

    - xml

--- app/src/main/java/com/example/myapplication/T09_XML/T09_XML.kt
+++ app/src/main/java/com/example/myapplication/T09_XML/T09_XML.kt
@@ -4,6 +4,11 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_t09__x_m_l.*
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.net.URL
+
 // http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153052000
 
 class T09_XML : AppCompatActivity() {
@@ -20,8 +25,37 @@ class T09_XML : AppCompatActivity() {
     }
 
     inner class WeatherTask:AsyncTask<String, Unit, String>(){
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            tvWeather.text = result
+        }
+
         override fun doInBackground(vararg params: String?): String {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val path = params[0]!!
+            val url = URL(path)
+
+            val factory = XmlPullParserFactory.newInstance()
+            val xpp = factory.newPullParser()
+            xpp.setInput(url.openStream(), "utf-8")
+
+            var bRead = false
+            var res = ""
+            var eventType = xpp.eventType
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                if(eventType == XmlPullParser.START_TAG){
+                    bRead = when(xpp.name){
+                        "hour", "day", "temp", "wfKor" -> true
+                        else -> false
+                    }
+                }else if(eventType == XmlPullParser.TEXT){
+                    if(bRead){
+                        res += xpp.text +"\n"
+                        bRead = false
+                    }
+                }
+                eventType = xpp.next()
+            }
+            return res
         }
 
     }
