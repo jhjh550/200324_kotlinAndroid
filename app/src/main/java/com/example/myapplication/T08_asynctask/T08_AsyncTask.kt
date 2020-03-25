f23d3d3762b27d0955a4026af0970cddda78c449
Your Name <you@example.com>
2020-03-25 13:47:37 +0900

    - asynctask

--- app/src/main/java/com/example/myapplication/T08_asynctask/T08_AsyncTask.kt
+++ app/src/main/java/com/example/myapplication/T08_asynctask/T08_AsyncTask.kt
@@ -3,8 +3,10 @@ package com.example.myapplication.T08_asynctask
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.activity_t08__async_task.*
+
 
 class T08_AsyncTask : AppCompatActivity() {
 
@@ -19,9 +21,25 @@ class T08_AsyncTask : AppCompatActivity() {
     }
 
     inner class MyTask: AsyncTask<Int, Float, String>(){
-        override fun doInBackground(vararg params: Int?): String {
-            params[0]
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            Toast.makeText(this@T08_AsyncTask,
+                result, Toast.LENGTH_SHORT).show()
         }
 
+        override fun onProgressUpdate(vararg values: Float?) {
+            super.onProgressUpdate(*values)
+            btnAsyncTask.text = "count ${values[0]}"
+        }
+
+        override fun doInBackground(vararg params: Int?): String {
+            val start:Int = params[0] ?: 0
+            for(i in start..100){
+                Thread.sleep(100)
+                publishProgress(i.toFloat())
+            }
+
+            return "Count is done."
+        }
     }
 }
