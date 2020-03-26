290e9309e0510fea7f807a856d6225795737c0ee
Your Name <you@example.com>
2020-03-26 13:35:38 +0900

    - retrofit

--- app/src/main/java/com/example/myapplication/t12_retrofit/T12_retrofit.kt
+++ app/src/main/java/com/example/myapplication/t12_retrofit/T12_retrofit.kt
@@ -2,12 +2,24 @@ package com.example.myapplication.t12_retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.myapplication.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class T12_retrofit : AppCompatActivity() {
-
+    data class MyPost(val userId:Int, val id:Int,
+                      val title:String, val body:String)
+    interface MyTypicodeInterface{
+        @GET("post")
+        fun getPosts(): Call<List<MyPost>>
+    }
+    lateinit var myTypicode:MyTypicodeInterface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_t12_retrofit)
@@ -17,7 +29,31 @@ class T12_retrofit : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-//        val myInstance = retrofit.create()
-//        myInstance.getPost()
+        myTypicode = retrofit.create(MyTypicodeInterface::class.java)
+        getPosts()
+    }
+
+    fun getPosts(){
+        val call = myTypicode.getPosts()
+        call.enqueue(object : Callback<List<MyPost>>{
+            override fun onFailure(call: Call<List<MyPost>>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<List<MyPost>>, response: Response<List<MyPost>>) {
+                if(response.isSuccessful == false){
+                    Toast.makeText(this@T12_retrofit,
+                        "error ${response.code()}", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                val posts = response.body()
+                if(posts != null) {
+                    for (post in posts) {
+                        Log.d("posts", post.toString())
+                    }
+                }
+            }
+
+        })
     }
 }
