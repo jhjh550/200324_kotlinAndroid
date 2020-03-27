ca470db4a4708e5c8f742184720a241d4e8c7ada
Your Name <you@example.com>
2020-03-27 14:52:50 +0900

    - style

--- app/src/main/java/com/example/myapplication/T20_style/T20_Style.kt
+++ app/src/main/java/com/example/myapplication/T20_style/T20_Style.kt
@@ -2,12 +2,29 @@ package com.example.myapplication.T20_style
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_t20__style.*
 
 class T20_Style : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_t20__style)
+
+        val numberListener = View.OnClickListener {
+            val btn = it as Button
+            val value = btn.text.toString()
+            val working = tvWorking.text.toString()
+            if(working == "0"){
+                tvWorking.text = value
+            }else{
+                tvWorking.text = "${working}${value}"
+            }
+        }
+
+        btnOne.setOnClickListener(numberListener)
+        btnZero.setOnClickListener(numberListener)
     }
 }
