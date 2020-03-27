ada7a9cef6ffe6c876ce19d52cd507f007dc5d63
Your Name <you@example.com>
2020-03-27 11:17:57 +0900

    - fragment

--- app/src/main/java/com/example/myapplication/T19_fragment/CounterFragment.kt
+++ app/src/main/java/com/example/myapplication/T19_fragment/CounterFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.fragment_counter.*
@@ -19,9 +21,13 @@ class CounterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val  v = inflater.inflate(R.layout.fragment_counter, container, false)
-
+        val v = inflater.inflate(R.layout.fragment_counter, container, false)
+        val btnIncrease = v.findViewById<Button>(R.id.btnIncrease)
+        val tvCounter = v.findViewById<TextView>(R.id.tvCounter)
+        btnIncrease.setOnClickListener {
+            val value = tvCounter.text.toString().toInt()
+            tvCounter.text = "${value+1}"
+        }
         return v
     }
 
