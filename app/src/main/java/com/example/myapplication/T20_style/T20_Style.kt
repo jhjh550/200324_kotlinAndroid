5f80934fb12372befe1a36269028168a46a53a45
Your Name <you@example.com>
2020-03-27 14:42:25 +0900

    - style

---
+++ app/src/main/java/com/example/myapplication/T20_style/T20_Style.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.T20_style
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class T20_Style : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_t20__style)
+    }
+}
